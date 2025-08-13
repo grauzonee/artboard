69650cf3123eac4cb13b8bb1a7f6fb71e06535ee
Dumm Devka <dummdevka@Dumms-MacBook-Air.local>
2025-08-13 12:07:19 +0200

    changes to back to sanitize imageUrl before changing it

--- backend/src/controllers/mediaController.ts
+++ backend/src/controllers/mediaController.ts
@@ -1,5 +1,6 @@
 import fs from 'fs/promises';
 import { existsSync } from 'fs';
+import { getConfigValue } from '@helper/configHelper';
 
 export const deleteImage = async (imagePath: string) => {
     await fs.unlink(imagePath);
@@ -7,4 +8,8 @@ export const deleteImage = async (imagePath: string) => {
 
 export const checkImage = (imagePath: string): boolean => {
     return existsSync(imagePath);
+}
+
+export const sanitizeImageUrl = (imageUrl: string): string => {
+    return imageUrl.replace(getConfigValue('HOST') + '/', '')
 }
